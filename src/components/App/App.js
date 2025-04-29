39ac3cc185a7b50a3ccb333464e729094db4c60f
Awesome6192 <christianchibuike14@gmail.com>
2025-04-29 09:16:04 +0100

    feat: improve layout responsiveness

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./App.css";
-import {HashRouter, Route} from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 
 import NavBar from "./../NavBar/NavBar.jsx";
 import Home from "./../Home/Home.jsx";
@@ -9,21 +9,20 @@ import Projects from "./../Projects/Projects.jsx";
 import Skills from "./../Skills/Skills.jsx";
 import Contact from "./../Contact/Contact.jsx";
 import GoHome from "./../GoHome/GoHome.jsx";
+
 function App() {
   return (
-    <div>
-      <HashRouter>
-        <div>
-          <NavBar/>
-          <Route path="/" exact component={Home}/>
-          <Route path="/about" exact component={About}/>
-          <Route path="/projects" exact component={Projects}/>
-          <Route path="/skills" exact component={Skills}/>
-          <Route path="/contact" exact component={Contact}/>
-          <GoHome/>
-        </div>
-      </HashRouter>
-    </div>
+    <HashRouter>
+      <div>
+        <NavBar />
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
+        <Route path="/projects" exact component={Projects} />
+        <Route path="/skills" exact component={Skills} />
+        <Route path="/contact" exact component={Contact} />
+        <GoHome />
+      </div>
+    </HashRouter>
   );
 }
 
